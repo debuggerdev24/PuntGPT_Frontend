c89e69ffd10453f400732350dd21a861c090319f
debuggerdev24 <debuggerdev.dds@gmail.com>
2026-05-04 13:03:11 +0530

    4th may

--- lib/screens/home/search_engine/web/widgets/search_section_web.dart
+++ lib/screens/home/search_engine/web/widgets/search_section_web.dart
@@ -178,7 +178,7 @@ class _SearchSectionWebState extends State<SearchSectionWeb> {
     required SearchEngineProvider provider,
     required bool isShowingFilterPanel,
   }) {
-    final cross = context.screenWidth > 980
+    final cross = context.screenWidth > 1010
         ? context.screenWidth > 1274
               ? isShowingFilterPanel
                     ? 3
