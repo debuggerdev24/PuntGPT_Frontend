783ef6fb5009a5a9505ae4de8eb8b6ed54d16f6b
debuggerdev24 <debuggerdev.dds@gmail.com>
2026-05-04 12:33:49 +0530

    4th may

--- lib/core/extensions/context_extensions.dart
+++ lib/core/extensions/context_extensions.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:web/web.dart' as web;
 
 extension BuildContextExtensions on BuildContext {
   double get screenHeight => MediaQuery.of(this).size.height;
   double get screenWidth => MediaQuery.of(this).size.width;
   double get bottomPadding => MediaQuery.paddingOf(this).bottom;
+  double get fullScreenWidth => web.window.screen.width.toDouble();
 }
--- lib/screens/home/search_engine/web/widgets/search_section_web.dart
+++ lib/screens/home/search_engine/web/widgets/search_section_web.dart
@@ -1,4 +1,3 @@
-import 'package:web/web.dart' as web;
 import 'package:puntgpt_nick/core/app_imports.dart';
 import 'package:modal_side_sheet/modal_side_sheet.dart';
 import 'package:puntgpt_nick/models/home/search_engine/search_model.dart';
@@ -55,7 +54,7 @@ class _SearchSectionWebState extends State<SearchSectionWeb> {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    if (context.screenWidth < web.window.screen.width - 60) ...[
+                    if (context.screenWidth < context.fullScreenWidth - 60) ...[
                       OnMouseTap(
                         onTap: _openFilterSideSheet,
                         child: Icon(Icons.tune_outlined, size: 20),
@@ -232,10 +231,10 @@ class _SearchSectionWebState extends State<SearchSectionWeb> {
           spacing: 6,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            if (context.screenWidth > web.window.screen.width - 60)
+            if (context.screenWidth > context.fullScreenWidth - 60)
               //* --------------------> left panel
               SizedBox(
-                width: 260,
+                width: (context.fullScreenWidth * 23) / 100,
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
